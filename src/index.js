c34863d716ae17a5e3e25cf1c2f7acf36b9a63c5
lucascr <lcrodrigues1995@gmail.com>
2022-07-05 15:23:53 -0300

    Install linters locally

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import _ from 'lodash';
 import './style.css';
 
 const tasks = [
-  { description: 'Wash the dishes', completed: true, index: 1},
-  { description: 'complete To Do list project', completed: false, index: 2},
+  { description: 'Wash the dishes', completed: true, index: 1 },
+  { description: 'complete To Do list project', completed: false, index: 2 },
 ];
 
 const markupAllTasks = () => {
   let list = '';
   tasks.forEach((task) => {
-    list += 
-  `<div class="id_${task.index}">
+    list
+  += `<div class="id_${task.index}">
     <input type="checkbox"  class="checkbox" ${task.completed ? 'checked' : ''}>
     <p>${task.description}</p>
     <button type="button">🗑</button>
@@ -18,11 +17,11 @@ const markupAllTasks = () => {
   <hr>`;
   });
   return list;
-}
+};
 
 const renderTasks = () => {
   const taskList = document.querySelector('.to-do-list');
   taskList.innerHTML = markupAllTasks();
-}
+};
 
 window.addEventListener('DOMContentLoaded', () => renderTasks());
