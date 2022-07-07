9e13650de767e3a3f065f51510f65a38333f9064
lucascr <lcrodrigues1995@gmail.com>
2022-07-07 16:56:39 -0300

    Put code in modules

--- src/index.js
+++ src/index.js
@@ -1,27 +1,4 @@
 import './style.css';
-
-const tasks = [
-  { description: 'Wash the dishes', completed: true, index: 1 },
-  { description: 'complete To Do list project', completed: false, index: 2 },
-];
-
-const markupAllTasks = () => {
-  let list = '';
-  tasks.forEach((task) => {
-    list
-  += `<div class="id_${task.index}">
-    <input type="checkbox"  class="checkbox" ${task.completed ? 'checked' : ''}>
-    <p>${task.description}</p>
-    <button type="button">🗑</button>
-  </div>
-  <hr>`;
-  });
-  return list;
-};
-
-const renderTasks = () => {
-  const taskList = document.querySelector('.to-do-list');
-  taskList.innerHTML = markupAllTasks();
-};
+const { renderTasks } = require('./modules/ux.js');
 
 window.addEventListener('DOMContentLoaded', () => renderTasks());
