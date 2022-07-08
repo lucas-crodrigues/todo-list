37cc857b0eabff304cc5e0d8049286fdee033ce7
lucascr <lcrodrigues1995@gmail.com>
2022-07-08 16:12:14 -0300

    Add functionality
    Add renderTasks and addEventListeners functions

--- src/modules/ux.js
+++ src/modules/ux.js
@@ -1,46 +1,43 @@
+const { markupAllTasks } = require('./functions.js');
 const { addTask } = require('./functions.js');
+const { removeTask } = require('./functions.js');
+const { updateTask } = require('./functions.js');
+const { updateIds } = require('./functions.js');
 
-const userAddTask = () => {
-  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  const input = document.querySelector('.add-task');
-  let iInput = input.value;
-  const id = tasks.length + 1;
-  tasks.push(addTask(iInput, id));
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-  renderTasks();
-  input.value = '';
+const renderTasks = () => {
+  const tasksContainer = document.querySelector('.todo-placeholder');
+  updateIds();
+  tasksContainer.innerHTML = markupAllTasks();
+  const page = document.querySelector('body');
+  page.replaceWith(page.cloneNode(true));
+  addEventListeners();
 }
 
 const addEventListeners = () => {
-  const form = document.querySelector('form');
-  form.addEventListener('submit', (e) => {
+  const addTaskForm = document.querySelector('form');
+  addTaskForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    userAddTask();
-  })
+    addTask();
+    renderTasks();
+  });
+
+  const removeTaskBtn = document.querySelectorAll('.remove');
+  removeTaskBtn.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      removeTask(e);
+      renderTasks();
+    });
+  });
+
+  const updateTaskInp = Array.from(document.querySelectorAll('.task-list .task-name'));
+  updateTaskInp.forEach((input) => {
+    input.addEventListener('input', (e) => {
+      updateTask(e);
+    });
+  });
 }
 
-const markupAllTasks = () => {
-  let list = '';
-  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  tasks.forEach((task) => {
-    list
-  += `<div class="id_${task.ID}">
-    <input type="checkbox"  class="checkbox" ${task.completed ? 'checked' : ''}>
-    <p>${task.description}</p>
-    <button type="button">🗑</button>
-  </div>
-  <hr>`;
-  });
-  return list;
-};
-
-const renderTasks = () => {
-  const taskList = document.querySelector('.to-do-list');
-  taskList.innerHTML = markupAllTasks();
-  addEventListeners();
-};
-
-module.exports = {
-  addEventListeners,
+module.exports = { 
   renderTasks,
+  addEventListeners
 }
