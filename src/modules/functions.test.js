427bc4f1b3b2fc52104fe4753ef93078b958946a
lucascr <lcrodrigues1995@gmail.com>
2022-07-14 07:53:09 -0300

    Create remove task function tests

--- src/modules/functions.test.js
+++ src/modules/functions.test.js
@@ -23,7 +23,7 @@ const html = `<section class="todo-list">
 
 document.body.innerHTML = html;
 
-describe('Add and delete functions', () => {
+describe('Add and remove functions', () => {
   test('add 1 item', () => {
     addTask();
     renderTasks();
@@ -31,12 +31,49 @@ describe('Add and delete functions', () => {
     expect(list).toHaveLength(1);
   });
 
-  test('add 2 items or more', () => {
+  test('add 2 more items', () => {
     addTask();
     renderTasks();
     addTask();
     renderTasks();
     const list = document.querySelectorAll('.task-list');
     expect(list).toHaveLength(3);
+  });
+
+  test('remove 1 item', () => {
+    let tasks = [ {taskName: "task 1", completed: false, id: 1}, {taskName: "task 2", completed: false, id: 2}, {taskName: "task 3", completed: false, id: 3} ];
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    renderTasks();
+    const removeTaskBtn = document.querySelectorAll('.remove');
+    removeTaskBtn.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        removeTask(e);
+        renderTasks();
+      });
+    });
+    document.querySelector('#r_3').click();
+    const localStorageTasks = JSON.parse(localStorage.getItem('tasks'));
+    const list = document.querySelectorAll('.task-list');
+    expect(localStorageTasks).toHaveLength(2);
+    expect(list).toHaveLength(2);
+  });
+
+  test('remove 2 items', () => {
+    let tasks = [ {taskName: "task 1", completed: false, id: 1}, {taskName: "task 2", completed: false, id: 2}, {taskName: "task 3", completed: false, id: 3} ];
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    renderTasks();
+    const removeTaskBtn = document.querySelectorAll('.remove');
+    removeTaskBtn.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        removeTask(e);
+        renderTasks();
+      });
+    });
+    document.querySelector('#r_3').click();
+    document.querySelector('#r_1').click();
+    const localStorageTasks = JSON.parse(localStorage.getItem('tasks'));
+    const list = document.querySelectorAll('.task-list');
+    expect(localStorageTasks).toHaveLength(1);
+    expect(list).toHaveLength(1);
   });
 });
